b0ef80a1d31bd80dd3a8dcc266ae7c0daf6739f1
SureshKuchana <kuchana123.suresh@gmail.com>
2021-01-09 19:46:39 +0530

    modified caa

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,11 @@ function IndexPage() {
     <Layout>
       <SEO title="Home" />
       <img src="/images/logos/logo.svg" alt="logo" />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
+      <h1>Build a web app with React Hooks</h1>
+      <p>
+        Learn how we built the new Design+Code site with React Hooks using
+        Gatsby, Netlify, and advanced CSS techniques with Styled Components. .
+      </p>
       <Link to="/page-2/">Go to page 2</Link> <br />
     </Layout>
   )
